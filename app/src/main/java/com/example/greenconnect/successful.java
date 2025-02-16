c598bd032f3472d57d1ff7fb4ae86a09e237c574
IreshikaSewwandi <sewwandiereshika@gmail.com>
2025-02-16 17:09:15 +0530

    Created signup successful page

---
+++ app/src/main/java/com/example/greenconnect/successful.java
@@ -0,0 +1,43 @@
+package com.example.greenconnect;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class successful extends AppCompatActivity {
+
+    TextView success;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_successful);
+
+
+        success = findViewById(R.id.continue_to);
+
+        success.setOnClickListener(v -> {
+            startActivity(new Intent(successful.this, login.class));
+        });
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
